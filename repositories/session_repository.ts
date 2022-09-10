9ea10946343f4a2b2680c18a6ea572c55036f64f
Eric Nograles <grales@gmail.com>
2022-09-10 23:14:14 +0000

    WIP: 🍪

--- repositories/session_repository.ts
+++ repositories/session_repository.ts
@@ -3,5 +3,8 @@ import { Client } from "replit-db";
 const db = new Client();
 
 export async function setSession(userId: string, token: Auth0TokenResponse) {
-  await db.set(userId, token);
+  let uuid = crypto.randomUUID();
+  let key = `${userId}_${uuid}`;
+  await db.set(key, token);
+  return key;
 }
--- routes/api/oauth/callback.ts
+++ routes/api/oauth/callback.ts
@@ -1,17 +1,18 @@
 import { Handlers } from "$fresh/server.ts";
 import { oauth2Client, getUserProfile } from 'services/oauth.ts'
 import { setSession } from 'repositories/session_repository.ts';
-import * as cookie from "cookie";
+import { setCookie } from "cookie";
 
 
 export const handler: Handlers = {
   async GET(req) {
-    const uuid = crypto.randomUUID();
     let oauthResponse = await oauth2Client.code.getToken(req.url);
     let profile = await getUserProfile(oauthResponse.accessToken);
-    let user = await setSession(profile.sub, oauthResponse);
-    return new Response(JSON.stringify({ ...oauthResponse }), {
+    let sessionId = await setSession(profile.sub, oauthResponse);
+    let response = new Response(JSON.stringify({ ...oauthResponse }), {
       headers: { "Content-Type": "application/json" },
     });
+    setCookie(response.headers, { name: 'session_id', value: sessionId, maxAge: 3600 })
+    return response;
   },
 };
